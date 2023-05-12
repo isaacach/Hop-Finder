5f24f8cefe152cb0a6feb3b5dc0d0dc3dace6062
isaacach <124728947+isaacach@users.noreply.github.com>
2023-05-11 22:56:11 -0500

    added beer cards and show functionality

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,12 +1,12 @@
-import { useEffect } from "react"
 
 function Search() {
+  const [beer, setBeer] = useState([]);
 
   const getBeer = async (beerName) => {
     const beer = await fetch(`https://api.punkapi.com/v2/beers?beer_name=${beerName}`)
     const beerJson = await beer.json()
     const filteredBeers = beerJson.filter((beer) => beer.name.toLowerCase.includes(beerName));
-    console.log(filteredBeers);
+    return setBeer(filteredBeers.map((b) => [...beer, b] ));
   }
 
 
@@ -20,7 +20,20 @@ function Search() {
           <h1>Or Search a Brewery</h1>
           <input className="brewerySearch" type="text" placeholder="Search..." />
         </div>
-        <div className='showBeer'></div>
+        <div className='showBeer'>
+          <h1>Beer Name</h1>
+          {beer.map((beer, i) => {
+            return (
+              <div className='beer' key={i}>
+                <h1>{beer.name}</h1>
+                <h2>{beer.tagline}</h2>
+                <h3>{beer.description}</h3>
+                <h4>{beer.abv}</h4>
+                <h5>{beer.ibu}</h5>
+                <h6>{beer.food_pairing}</h6>
+                </div>
+            )})}
+        </div>
       </div>
     </div>
   )
